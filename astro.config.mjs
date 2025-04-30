063685f92ae447f2ca1eeda1ad0b7420e89ed4fc
arthur.salahutdinov <artes_93@mail.ru>
2025-05-01 00:37:57 +0300

    🎉 Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/site_portfolio/',
   vite: {
     plugins: [tailwindcss()]
   }
